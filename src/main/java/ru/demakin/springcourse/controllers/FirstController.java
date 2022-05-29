6a4528b41e9694b5bb776079498eca40bb2633e4
Stepashkin63 <spetc_kill@mail.ru>
2022-05-29 20:57:34 +0400

    add Spring Web Application

---
+++ src/main/java/ru/demakin/springcourse/controllers/FirstController.java
@@ -0,0 +1,18 @@
+package ru.demakin.springcourse.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FirstController {
+
+    @GetMapping("/hello")
+    public String helloPAge() {
+        return "first/hello";
+    }
+
+    @GetMapping("/goodbye")
+    public String goodByePage() {
+        return "first/goodbye";
+    }
+}
